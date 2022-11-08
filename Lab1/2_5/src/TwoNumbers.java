3e68ea315969ab9d71334c7f790180f9584be0b9
hieu748159263 <hieu748159263@gmail.com>
2022-11-08 18:15:01 +0700

    lab1 init

---
+++ Lab1/2_5/src/TwoNumbers.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        Double num1, num2;
+
+        num1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Please input the first number:"));
+        num2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Please input the second number:"));
+
+        Double sum = num1 + num2;
+        Double diff = num1 - num2;
+        Double product = num1 * num2;
+        String msg = String.format("Numbers inputted: %f and %f\n"
+                + "Sum: %.2f\n"
+                + "Difference: %.2f\n"
+                + "Product: %.2f\n", num1, num2, sum, diff, product);
+
+        if (num2 == 0) {
+            msg += "Can't devide when divisor is zero!";
+        } else {
+            msg += String.format("Quotient: %.2f", num1/num2);
+        }
+
+        JOptionPane.showMessageDialog(null, msg);
+
+        System.exit(0);
+    }
+}
---
+++ Lab1/6_5/src/array_program.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class array_program {
+    public static void main(String[] args) throws Exception {
+        // sort a numeric array
+        // and calculate the sum and average value of array elements.
+        int[] iArray = { 1789, 2035, 1899, 1456, 2013, 2002 };
+        Arrays.sort(iArray);
+        int sum = 0;
+        for (int i : iArray) {
+            sum += i;
+        }
+        double avg = sum / iArray.length;
+
+        // Print the array, sum and avg
+        System.out.print("Sorted array: ");
+        for (int i : iArray) {
+            System.out.print(i + " ");
+        }
+        System.out.format("\nArray sum: %d\n" +
+                "Average value: %.2f", sum, avg);
+    }
+}
