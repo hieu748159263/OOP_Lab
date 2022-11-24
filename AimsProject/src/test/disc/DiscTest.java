8a41d0dfd67ac758c155aa93aa27cf360a26adb8
hieu748159263 <hieu748159263@gmail.com>
2022-11-24 22:42:10 +0700

    test toString function

---
+++ AimsProject/src/test/disc/DiscTest.java
@@ -0,0 +1,11 @@
+package test.disc;
+
+import test.dvdObjects;
+
+public class DiscTest extends dvdObjects {
+    public static void main(String[] args) {
+
+        // Lab3: test toString function
+        System.out.println(dvd1.toString());
+    }
+}
