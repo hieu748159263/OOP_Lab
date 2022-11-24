7f235eea208a25dbe34e795ace34188697d6edfd
hieu748159263 <hieu748159263@gmail.com>
2022-11-24 21:51:28 +0700

    refactor of lab1 and lab2 work

---
+++ OtherProjects/lab1/src/Ex62/InputFromKeyboard.java
@@ -0,0 +1,20 @@
+package Ex62;
+
+import java.util.Scanner;
+
+public class InputFromKeyboard {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String strName = keyboard.nextLine();
+        System.out.println("How old are you?");
+        int iAge = keyboard.nextInt();
+        System.out.println("How tall are you (m)?");
+        double dHeight = keyboard.nextDouble();
+
+        keyboard.close();
+
+        System.out.format("Mrs/Ms. %s, %d years old. Your height is %.2f(m).", strName, iAge, dHeight);
+    }
+}
