4b6ad18481acadbcc94673f68f5a943c97eedc25
Nguyen Hieu <hieu748159263@gmail.com>
2022-12-09 02:21:00 +0700

    Refactor of StoreTest.java (#10)

--- AimsProject/src/test/store/StoreTest.java
+++ AimsProject/src/test/store/StoreTest.java
@@ -1,9 +1,9 @@
 package test.store;
 
 import aims.store.Store;
-import test.media.mediaObjects;
+import test.media.MediaObjects;
 
-public class StoreTest extends mediaObjects {
+public class StoreTest extends MediaObjects {
     public static void main(String[] args) {
 
         // Lab3: test addDvd and removeDvd methods
