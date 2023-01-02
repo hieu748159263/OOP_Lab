ac3ad6bae7d9afdd2d37827b55d297bec4d36d33
hieu748159263 <hieu748159263@gmail.com>
2023-01-02 23:02:05 +0700

    Implementations of basic paint app

--- GUIProject/src/main/java/module-info.java
+++ GUIProject/src/main/java/module-info.java
@@ -1,4 +1,11 @@
-module hust.soict.swing {
+module hust.soict {
     requires java.desktop;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+
+    opens hust.soict.javafx to javafx.fxml;
+
+    exports hust.soict.javafx;
     exports hust.soict.swing;
 }
