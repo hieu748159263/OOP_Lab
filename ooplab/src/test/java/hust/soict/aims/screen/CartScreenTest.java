1aa69e6f5291d4f67d6519f8e72fcec7da2f7a22
hieu748159263 <hieu748159263@gmail.com>
2023-01-06 20:40:21 +0700

    Delete redundant directories

--- ooplab/src/test/java/hust/soict/aims/screen/CartScreenTest.java
+++
@@ -1,9 +0,0 @@
-package hust.soict.aims.screen;
-
-import hust.soict.aims.cart.Cart;
-
-public class CartScreenTest {
-    public static void main(String[] args) {
-        new CartScreen(new Cart());
-    }
-}
